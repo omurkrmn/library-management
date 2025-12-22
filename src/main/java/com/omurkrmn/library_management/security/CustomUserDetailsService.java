66e870c971a13fccb372af292aa81bfda91850dd
omurkrmn <ahmetomurkaraman06@gmail.com>
2025-12-22 14:14:56 +0300

    Jwt update.

---
+++ src/main/java/com/omurkrmn/library_management/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.omurkrmn.library_management.security;
+
+import com.omurkrmn.library_management.entity.User;
+import com.omurkrmn.library_management.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User uSer = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(uSer.getUsername())
+                .password(uSer.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
