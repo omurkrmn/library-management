e1015839d6bce20e6e45d564212915de4ebe7916
omurkrmn <ahmetomurkaraman06@gmail.com>
2025-12-22 14:43:43 +0300

    Jwt update.

--- src/main/java/com/omurkrmn/library_management/config/JwtConfig.java
+++ src/main/java/com/omurkrmn/library_management/config/JwtConfig.java
@@ -3,19 +3,23 @@ package com.omurkrmn.library_management.config;
 
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+
 @Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
 public class JwtConfig {
 
-    @Value("${jwt.secret}")
+
     private String secret;
 
-    @Getter
-    @Value("${jwt.expiration}")
     private long expiration;
 
     public Key getKey() {
